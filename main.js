bdb95298df3969ce4910b0197014778d10ae355a
yc62897441 <yc62897441@gmail.com>
2022-10-26 19:41:50 +0800

    改用 checkbox

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+const body = document.querySelector('body')
 const select = document.querySelector('select')
 const options = document.querySelectorAll('option')
+const checkboxGroup = document.createElement("div")
+
+options.forEach(option => {
+  checkboxGroup.innerHTML += `<label style="display: block;"><input type="checkbox">${option.text}</label>`
+  option.hidden = true
+  option.selected = false
+  option.disabled = true
+})
 
 // 建立新元素，當作 select 收合時的顯示選項
 const headOption = document.createElement("option")
@@ -8,25 +17,46 @@ headOption.text = defaultText
 select.insertBefore(headOption, options[0])
 headOption.selected = true
 headOption.disabled = true
-headOption.hidden = true
+
+// 在 DOM 上插入 checkbox
+function insertAfter(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+insertAfter(checkboxGroup, select)
+checkboxGroup.style.width = '120px'
+checkboxGroup.style.display = 'none'
+
+// 控制 checkbox 顯示/隱藏
+let isShow = false
+select.addEventListener('mousedown', function (event) {
+  event.preventDefault()
+  if (!isShow) {
+    checkboxGroup.style.display = 'block'
+    isShow = true
+  } else {
+    checkboxGroup.style.display = 'none'
+    isShow = false
+  }
+})
+checkboxGroup.addEventListener('mouseleave', function () {
+  if (isShow) {
+    checkboxGroup.style.display = 'none'
+    isShow = false
+  }
+})
 
 // 有被選取的選項
 let optionsInHeadOption = []
-
-select.addEventListener('input', function () {
+checkboxGroup.addEventListener('input', function (event) {
   // 被選取選項的文字內容
-  const inputOptionText = this.options[this.selectedIndex].text
+  const inputOptionText = event.target.parentElement.textContent
 
   // 有被選取的選項，上底色；取消選取時，移除底色
-  if (this.options[this.selectedIndex].style.backgroundColor === '') {
-    this.options[this.selectedIndex].style.backgroundColor = '#999999'
+  if (event.target.parentElement.style.backgroundColor === '') {
+    event.target.parentElement.style.backgroundColor = '#999999'
   } else {
-    this.options[this.selectedIndex].style.backgroundColor = ''
+    event.target.parentElement.style.backgroundColor = ''
   }
-
-  // 維持選取欄收合時，都是顯示 headOption
-  this.options.selected = false
-  headOption.selected = true
 
   // 確認「被選取選項的文字內容」是否已經在 optionsInHeadOption 之中；如果是，則從 optionsInHeadOption 刪除該項；如果否，則把該項加入 optionsInHeadOption
   let isInOptionsInHeadOption = false
